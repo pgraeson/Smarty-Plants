de9013f7e28cf66029434d9801768a0b2aefbfa8
pgraeson <30897496+pgraeson@users.noreply.github.com>
2017-10-10 22:05:13 +1100

    Add files via upload

---
+++ Smarty_Plants.ino
@@ -0,0 +1,26 @@
+int value = 0;
+
+void setup() {
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(1200);
+}
+
+void loop() {
+//Serial.println("HELLO");
+  value = analogRead(0);
+  Serial.println(value);
+if (value < 300) 
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+   
+}  
+
+
+else
+{
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+ delay(1000);
+}
